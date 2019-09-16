1dce5b1e45f6c269b8a37bdc1ba59b3a58b7ce2f
corne12345 <39339251+corne12345@users.noreply.github.com>
2019-09-16 14:13:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 import ta
 import sa
+
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import pandas as pd
+import numpy as np
+import tarfile
+import json
+import matplotlib.pyplot as plt
+
+tf = tarfile.open('geotagged_tweets_20160812-0912.tar.gz', mode='r')
+tf.extractall()
+counter = 0
+tweets_dict = {}
+with open('geotagged_tweets_20160812-0912.jsons') as fd:
+    for line in fd:
+        j_content = json.loads(line)
+        tweets_dict[counter] = j_content
+        counter += 1
+        if counter == 1000: # to keep it  small
+            break
+            
+df = pd.DataFrame.from_dict(tweets_dict, orient='index')
+df.head()
