7fcac5be42b26a115075b7aebeec7da07c375d15
corne12345 <39339251+corne12345@users.noreply.github.com>
2019-09-16 15:49:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -33,3 +33,44 @@ cnt = Counter()
 for i in range(df['place'].shape[0]):
     if df['place'].iloc[i]['country_code'] == 'US':
         cnt[df['place'].iloc[i]['full_name'][-2:]] += 1
+
+# Next step is to make a map visualization of the distribution        
+import os
+from shapely.geometry import Polygon, MultiPolygon
+from descartes import PolygonPatch
+from matplotlib import cm
+import math
+
+S_DIR = 'states_21basic/' 
+MAXIMUM = cnt_df.max() # get the highest value state
+BLUE = '#5599ff'
+cmap = plt.get_cmap('YlOrRd') # Choose a colormap to be used to color
+
+with open(os.path.join(S_DIR, 'states.json')) as rf:    
+    data = json.load(rf)
+
+fig = plt.figure() 
+ax = fig.gca()
+for feature in data['features']:
+    geometry = feature['geometry']
+    
+    # Convert population to a proportion of the maximum value
+    name = feature['properties']['STATE_ABBR']
+    frequency_prop = math.sqrt((cnt[name]/MAXIMUM))
+    
+    if geometry['type'] == 'Polygon':
+        poly = geometry
+        ppatch = PolygonPatch(poly, fc=cmap(frequency_prop), ec=BLUE,  alpha=0.5, zorder=2)
+        ax.add_patch(ppatch)
+
+    elif geometry['type'] == 'MultiPolygon':
+        for polygon in geometry['coordinates'][0]:
+            poly = Polygon(polygon)
+            ppatch = PolygonPatch(poly, fc=cmap(frequency_prop), ec=BLUE, alpha=0.5, zorder=2)
+            ax.add_patch(ppatch)
+    else:
+        print('Don\'t know how to draw :', geometry['type'])
+
+ax.axis('scaled')
+plt.axis('off')
+plt.show()
